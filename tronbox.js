ac5a78c3d6bae6036caaa6af2cf6fcc57273a129
yulian-khalitov <khalitov.yulian@yandex.ru>
2021-01-27 01:56:26 +0300

    fix contract verification issue

--- tronbox.js
+++ tronbox.js
@@ -6,6 +6,17 @@ module.exports = {
       fee_limit: 100000000,
       fullHost: "http://127.0.0.1:9090",
       network_id: "*"
+    },
+    compilers: {
+        solc: {
+            version: '0.4.25' // for compiler version
+        }
     }
+  },
+  solc: {
+      optimizer: {
+          enabled: true, // enable solc optimize, default: false
+          runs: 200
+      }
   }
 };
